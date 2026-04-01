2bb56f5628fe3a1c404de182e88c35e9dc7f97f1
Felipe Moreira <felipemoreira.frontend@gmail.com>
2026-04-01 10:16:24 -0300

    implementado objeto de criar pessoa (#2)

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello & Welcome to JavaScript");
+const pessoa =  {
+    nome: "Matheus",
+    idade: 23,
+    cidade: "São José do Belmonte"
+}
+
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.cidade);
