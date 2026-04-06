5f7b685c709801e706aa1cad6442def34b7211c9
Matheus Amorim <amorim.cpm@gmail.com>
2026-04-06 19:11:00 -0300

    exercicio 8

---
+++ ex008.js
@@ -0,0 +1,9 @@
+// console.log("O console funcionou corretamente!")
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} Km/h!`)
+
+if (vel > 60) { //condição simples
+    console.log('A velocidade está acima da permitida, MULTADO!')
+}
+
+console.log('Dirija sempre usando  o cinto de segurança!')
