1a5324240a5d90cc832195320fb54012ef6f837c
Matheus Amorim <amorim.cpm@gmail.com>
2026-04-10 21:14:39 -0300

    aula 16 funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,14 @@
+function parimpar(n) {
+    if (n % 2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+console.log(parimpar(223)) // exibir o resultado da função
+
+/* guardar na variavel e exibir essa variavel
+let res = parimpar(4)
+console.log(res)
+*/
---
+++ aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma (n1=0, n2=0) { //paremetros pré-definidos/opicionais
+    return n1 + n2
+}
+
+console.log(soma(2, 5))
